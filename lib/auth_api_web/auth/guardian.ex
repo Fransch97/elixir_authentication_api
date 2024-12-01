5bd8d6f5fb43f8c1a5bc470f7160280c46c21704
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2024-12-01 22:59:00 +0100

    added authentication on guardian

--- lib/auth_api_web/auth/guardian.ex
+++ lib/auth_api_web/auth/guardian.ex
@@ -21,4 +21,24 @@ defmodule AuthApi.Auth.Guardian do
   def resource_from_claims(_claims) do
     {:error, :no_id_provided}
   end
+
+  def authenticate(email, password) do
+    case Accounts.get_account_by_email(email) do
+      nil -> {:error, :unauthorized}
+      account ->
+        case validate_password(password, account.hash_password) do
+          true -> create_token(account)
+          false -> {:error, :unauthorized}
+        end
+    end
+  end
+
+  defp validate_password(password, hash_password) do
+    Bcrypt.verifiy_oass(password, hash_password)
+  end
+
+  defp create_token(account) do
+    {:ok, token, _claims} = encode_and_sign(account)
+    {:ok, account, token}
+  end
 end
